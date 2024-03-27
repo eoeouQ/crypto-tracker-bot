74f870b07be7cbfc3d85dde52d3fdec855f1ba3c
Timofey Kozlov <iZouiR@jotaro.ru>
2024-03-27 16:43:14 +0300

    update BotUpdateService: add command_subscribe, command_unsubscribe support

--- src/main/java/org/izouir/crypto_tracker_bot/service/bot/BotUpdateService.java
+++ src/main/java/org/izouir/crypto_tracker_bot/service/bot/BotUpdateService.java
@@ -2,9 +2,11 @@ package org.izouir.crypto_tracker_bot.service.bot;
 
 import org.izouir.crypto_tracker_bot.service.AuthorizationService;
 import org.izouir.crypto_tracker_bot.service.CryptoService;
+import org.izouir.crypto_tracker_bot.service.SubscriptionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
@@ -19,14 +21,17 @@ public class BotUpdateService {
     private final BotMessageService botMessageService;
     private final AuthorizationService authorizationService;
     private final CryptoService cryptoService;
+    private final SubscriptionService subscriptionService;
 
     @Autowired
     public BotUpdateService(final BotMessageService botMessageService,
                             final AuthorizationService authorizationService,
-                            final CryptoService cryptoService) {
+                            final CryptoService cryptoService,
+                            final SubscriptionService subscriptionService) {
         this.botMessageService = botMessageService;
         this.authorizationService = authorizationService;
         this.cryptoService = cryptoService;
+        this.subscriptionService = subscriptionService;
     }
 
     public void executeUpdateAsync(final TelegramLongPollingBot bot, final Update update) {
@@ -42,8 +47,17 @@ public class BotUpdateService {
                         authorizationService.authorizeUser(bot, chatId, username);
                     }
                     case COMMAND_TOP -> cryptoService.sendTopCryptoCurrencies(bot, chatId);
+                    case COMMAND_SUBSCRIBE -> subscriptionService.subscribe(bot, chatId);
+                    case COMMAND_UNSUBSCRIBE -> subscriptionService.unsubscribe(bot, chatId);
                     default -> botMessageService.sendAsync(bot, chatId, COMMAND_UNKNOWN_MESSAGE);
                 }
+            } else if (update.hasCallbackQuery()) {
+                final CallbackQuery callbackQuery = update.getCallbackQuery();
+                final Long chatId = callbackQuery.getMessage().getChatId();
+                final Integer messageId = callbackQuery.getMessage().getMessageId();
+                final String callbackData = callbackQuery.getData();
+
+                subscriptionService.executeCallback(bot, chatId, messageId, callbackData);
             }
         });
     }
