39ed3e2f7d294e0d5853e8b68eba93123fefd838
Timofey Kozlov <iZouiR@jotaro.ru>
2024-03-27 16:37:10 +0300

    update CryptoTrackerBotApplication: enable scheduling

--- src/main/java/org/izouir/crypto_tracker_bot/CryptoTrackerBotApplication.java
+++ src/main/java/org/izouir/crypto_tracker_bot/CryptoTrackerBotApplication.java
@@ -2,7 +2,9 @@ package org.izouir.crypto_tracker_bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class CryptoTrackerBotApplication {
 
