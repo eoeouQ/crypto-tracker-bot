f92a9f03fd8dfb7297967931e177fd72d51b0359
Timofey Kozlov <iZouiR@jotaro.ru>
2024-03-27 18:26:26 +0300

    update SubscriptionSchedulerConstant

--- src/main/java/org/izouir/crypto_tracker_bot/util/constant/scheduler/SubscriptionSchedulerConstant.java
+++ src/main/java/org/izouir/crypto_tracker_bot/util/constant/scheduler/SubscriptionSchedulerConstant.java
@@ -1,5 +1,6 @@
 package org.izouir.crypto_tracker_bot.util.constant.scheduler;
 
 public class SubscriptionSchedulerConstant {
-    public static final String NOTIFY_MESSAGE = "Срочно! %s - %s$ (%s%%)";
+    public static final Double MIN_CRYPTO_CURRENCY_PRICE = 0.01;
+    public static final String NOTIFY_MESSAGE = "Срочно❗ %s = %s$ (%s%%)";
 }
