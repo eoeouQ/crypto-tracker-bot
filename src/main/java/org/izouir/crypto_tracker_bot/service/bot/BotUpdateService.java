346c8f601144cdd2b3c924d07302d9d44b9d7f48
Timofey Kozlov <iZouiR@jotaro.ru>
2024-03-27 04:35:44 +0300

    update BotUpdateService: add command_start support

--- src/main/java/org/izouir/crypto_tracker_bot/service/bot/BotUpdateService.java
+++ src/main/java/org/izouir/crypto_tracker_bot/service/bot/BotUpdateService.java
@@ -1,28 +1,45 @@
 package org.izouir.crypto_tracker_bot.service.bot;
 
+import org.izouir.crypto_tracker_bot.service.AuthorizationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import static org.izouir.crypto_tracker_bot.util.constant.service.bot.BotUpdateServiceConstant.*;
+
 @Service
 public class BotUpdateService {
     private static final ExecutorService EXECUTOR = Executors.newWorkStealingPool();
     private final BotMessageService botMessageService;
+    private final AuthorizationService authorizationService;
 
     @Autowired
-    public BotUpdateService(final BotMessageService botMessageService) {
+    public BotUpdateService(final BotMessageService botMessageService,
+                            final AuthorizationService authorizationService) {
         this.botMessageService = botMessageService;
+        this.authorizationService = authorizationService;
     }
 
     public void executeUpdateAsync(final TelegramLongPollingBot bot, final Update update) {
         EXECUTOR.execute(() -> {
-            final Long chatId = update.getMessage().getChatId();
-            final String text = update.getMessage().getText();
-            botMessageService.sendAsync(bot, chatId, text);
+            if (update.hasMessage() && update.getMessage().hasText()) {
+                final Message message = update.getMessage();
+                final Long chatId = message.getChatId();
+                final String text = message.getText();
+
+                switch (text) {
+                    case COMMAND_START -> {
+                        final String username = message.getChat().getUserName();
+                        authorizationService.authorizeUser(bot, chatId, username);
+                    }
+                    default -> botMessageService.sendAsync(bot, chatId, COMMAND_UNKNOWN_MESSAGE);
+                }
+            }
         });
     }
 }
