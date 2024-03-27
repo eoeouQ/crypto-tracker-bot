2ac611bde6a076af4db0ab934ddc749983888702
Timofey Kozlov <iZouiR@jotaro.ru>
2024-03-27 16:42:25 +0300

    update BotUpdateServiceConstant

--- src/main/java/org/izouir/crypto_tracker_bot/util/constant/service/bot/BotUpdateServiceConstant.java
+++ src/main/java/org/izouir/crypto_tracker_bot/util/constant/service/bot/BotUpdateServiceConstant.java
@@ -3,5 +3,7 @@ package org.izouir.crypto_tracker_bot.util.constant.service.bot;
 public class BotUpdateServiceConstant {
     public static final String COMMAND_START = "/start";
     public static final String COMMAND_TOP = "/top";
+    public static final String COMMAND_SUBSCRIBE = "/subscribe";
+    public static final String COMMAND_UNSUBSCRIBE = "/unsubscribe";
     public static final String COMMAND_UNKNOWN_MESSAGE = "Неизвестная комманда";
 }
