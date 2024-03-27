3df6e3c64d7906a46a628a3b992f1a9828a13f57
Timofey Kozlov <iZouiR@jotaro.ru>
2024-03-27 16:40:52 +0300

    add SubscriptionServiceKeyboardConstant

---
+++ src/main/java/org/izouir/crypto_tracker_bot/util/constant/keyboard/service/SubscriptionServiceKeyboardConstant.java
@@ -0,0 +1,5 @@
+package org.izouir.crypto_tracker_bot.util.constant.keyboard.service;
+
+public class SubscriptionServiceKeyboardConstant {
+    public static final String SUBSCRIBER_PERCENT_BUTTON_TEXT_FORMAT = "%s%%";
+}
