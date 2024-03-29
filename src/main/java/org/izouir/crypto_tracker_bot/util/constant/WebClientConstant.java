1e5560e4960033e539207db370a9cd9270d68418
Timofey Kozlov <iZouiR@jotaro.ru>
2024-03-29 16:03:03 +0300

    update WebClientConstant

--- src/main/java/org/izouir/crypto_tracker_bot/util/constant/WebClientConstant.java
+++ src/main/java/org/izouir/crypto_tracker_bot/util/constant/WebClientConstant.java
@@ -1,6 +1,7 @@
 package org.izouir.crypto_tracker_bot.util.constant;
 
 public class WebClientConstant {
-    public static final int TIMEOUT = 1000;
+    public static final Integer TIMEOUT = 3000;
     public static final String BASE_URL = "https://api.mexc.com/api/v3/ticker/price";
+    public static final String USDT_SUFFIX = "USDT";
 }
